b1bc42d2fb9521c53de7141e51ab973305fdb464
dhkim <dhkim@gmail.com>
2016-12-05 11:51:07 +0900

    BoardGenreVO -> HaveBaordVO로 변경, BoardGenreVO 새로 추가, SignBoardVO,WorkVO 변경

--- ganpan/src/main/java/org/sync/ganpan/model/vo/WorkVO.java
+++ ganpan/src/main/java/org/sync/ganpan/model/vo/WorkVO.java
@@ -6,72 +6,88 @@ public class WorkVO {
 	private String workDetails;
 	private String creationDate;
 	private String changeDate;
-	private BoardGenreVO boardGenreVO;//boardNo
-	//workerNickName==memberVO.getNickName() , signBoard.getSignBoardName() | getBossMemberVO()
+	private HaveBoardVO haveBoardVO;
 	private OrganizationVO organizationVO;
 	
 	public WorkVO() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public WorkVO(int workNo, String workName, String workDetails, String creationDate, String changeDate,
-			BoardGenreVO boardGenreVO, OrganizationVO organizationVO) {
+			HaveBoardVO haveBoardVO, OrganizationVO organizationVO) {
 		super();
 		this.workNo = workNo;
 		this.workName = workName;
 		this.workDetails = workDetails;
 		this.creationDate = creationDate;
 		this.changeDate = changeDate;
-		this.boardGenreVO = boardGenreVO;
+		this.haveBoardVO = haveBoardVO;
 		this.organizationVO = organizationVO;
 	}
+
 	public int getWorkNo() {
 		return workNo;
 	}
+
 	public void setWorkNo(int workNo) {
 		this.workNo = workNo;
 	}
+
 	public String getWorkName() {
 		return workName;
 	}
+
 	public void setWorkName(String workName) {
 		this.workName = workName;
 	}
+
 	public String getWorkDetails() {
 		return workDetails;
 	}
+
 	public void setWorkDetails(String workDetails) {
 		this.workDetails = workDetails;
 	}
+
 	public String getCreationDate() {
 		return creationDate;
 	}
+
 	public void setCreationDate(String creationDate) {
 		this.creationDate = creationDate;
 	}
+
 	public String getChangeDate() {
 		return changeDate;
 	}
+
 	public void setChangeDate(String changeDate) {
 		this.changeDate = changeDate;
 	}
-	public BoardGenreVO getBoardGenreVO() {
-		return boardGenreVO;
+
+	public HaveBoardVO getHaveBoardVO() {
+		return haveBoardVO;
 	}
-	public void setBoardGenreVO(BoardGenreVO boardGenreVO) {
-		this.boardGenreVO = boardGenreVO;
+
+	public void setHaveBoardVO(HaveBoardVO haveBoardVO) {
+		this.haveBoardVO = haveBoardVO;
 	}
+
 	public OrganizationVO getOrganizationVO() {
 		return organizationVO;
 	}
+
 	public void setOrganizationVO(OrganizationVO organizationVO) {
 		this.organizationVO = organizationVO;
 	}
+
 	@Override
 	public String toString() {
 		return "WorkVO [workNo=" + workNo + ", workName=" + workName + ", workDetails=" + workDetails
-				+ ", creationDate=" + creationDate + ", changeDate=" + changeDate + ", boardGenreVO=" + boardGenreVO
+				+ ", creationDate=" + creationDate + ", changeDate=" + changeDate + ", haveBoardVO=" + haveBoardVO
 				+ ", organizationVO=" + organizationVO + "]";
 	}
+	
 	
 }//class WorkVO
