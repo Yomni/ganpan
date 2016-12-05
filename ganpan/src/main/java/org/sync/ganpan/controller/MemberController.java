069ee5e9bb216b7df57d41f06ec247b09b39ec24
JYM <yearsheep@naver.com>
2016-12-05 16:57:32 +0900

    로그인된 화면 작성, 로그인 후 화면 으로 가도록 변경

--- ganpan/src/main/java/org/sync/ganpan/controller/MemberController.java
+++ ganpan/src/main/java/org/sync/ganpan/controller/MemberController.java
@@ -1,6 +1,5 @@
 package org.sync.ganpan.controller;
 
-
 import java.util.HashMap;
 
 import javax.annotation.Resource;
@@ -25,8 +24,8 @@ import org.sync.ganpan.model.vo.MemberVO;
 public class MemberController {
 	@Resource
 	private MemberService memberService;
-	
-/****************************************민영**********************************************/
+
+	/****************************************민영**********************************************/
 	/**
 	 * 회원 가입
 	 * @author 민영
@@ -38,7 +37,7 @@ public class MemberController {
 		request.getSession().setAttribute("mvo", mvo);
 		return "redirect:registerResultView.do?nickName=" + mvo.getNickName();
 	}
-	
+
 	/**
 	 * 회원가입 결과화면으로 이동
 	 * @author 민영
@@ -49,7 +48,7 @@ public class MemberController {
 		MemberVO mvo = memberService.findMemberByNickName(nickName);
 		return new ModelAndView("member/register_result", "mvo", mvo);
 	}
-	
+
 	/**
 	 * 회원가입 시 이메일 중복확인
 	 * @author 민영
@@ -72,10 +71,9 @@ public class MemberController {
 		int count = memberService.nickNameCheck(nickName);
 		return (count == 0) ? "ok" : "fail";
 	}
-	
-/******************************************************************************************/
-	
-	
+
+	/******************************************************************************************/
+
 	/******************************** 주선 **********************************/
 	/**
 	 * 회원 로그인
@@ -96,15 +94,15 @@ public class MemberController {
 		System.out.println(mvo2);
 		if (mvo != null) {
 			request.getSession().setAttribute("mvo", mvo);
-			return "redirect:homeSignBoardList.do";
-		} else if(mvo2 != null){
+			return "redirect:home_login.do";
+		} else if (mvo2 != null) {
 			request.getSession().setAttribute("mvo", mvo2);
-			return "redirect:homeSignBoardList.do";
-		} else{
+			return "redirect:home_login.do";
+		} else {
 			return "redirect:member/login_fail.do";
 		}
 	}
-	
+
 	/**
 	 * 회원 로그아웃
 	 * @author 주선
@@ -117,6 +115,7 @@ public class MemberController {
 			session.invalidate();
 		return "redirect:gohome.do";
 	}
+
 	/************************************************************************/
 
 	/**
@@ -128,12 +127,8 @@ public class MemberController {
 		int check = memberService.updateMember(mvo);
 		System.out.println(check);
 		request.getSession().setAttribute("mvo", mvo);
-		//return "registerResultView.do?nickName=" + mvo.getNickName();
-		System.out.println("updateMember method mvo.getNickName() : "+mvo.getNickName());
+		// return "registerResultView.do?nickName=" + mvo.getNickName();
+		System.out.println("updateMember method mvo.getNickName() : " + mvo.getNickName());
 		return "redirect:member/my_info.do";
 	}
-	
 }
-
-
-	
