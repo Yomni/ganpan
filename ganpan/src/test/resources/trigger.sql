4847e38f63a74b88dd866bdc393ed92914d47054
Kang <Kang@192.168.0.132>
2016-12-07 11:46:44 +0900

    trigger.sql 추가

---
+++ ganpan/src/test/resources/trigger.sql
@@ -0,0 +1,66 @@
+<!--
+CREATE 부터 END 하나씩 까지 끌어서
+시작프로그램 -> Database 홈페이지로 이동 -> final / project 입력 후
+SQL -> SQL명령에서 실행해 주세요
+-->
+
+DROP TRIGGER sign_board_org_trigger;
+DROP TRIGGER sb_sbname_invite_trigger;
+DROP TRIGGER sb_sbname_have_trigger;
+DROP TRIGGER sb_bossnickname_org_trigger;
+DROP TRIGGER sb_bossnickname_invite_trigger;
+DROP TRIGGER sb_bossnickname_have_trigger;
+
+
+CREATE OR REPLACE TRIGGER sb_sbname_org_trigger
+AFTER UPDATE ON sign_board FOR EACH ROW
+BEGIN
+  UPDATE organization
+  SET sign_board_name=:NEW.sign_board_name
+  WHERE sign_board_name=:OLD.sign_board_name;
+END;
+
+
+CREATE OR REPLACE TRIGGER sign_board_invite_trigger
+AFTER UPDATE ON sign_board FOR EACH ROW
+BEGIN
+  UPDATE invitation_management
+  SET sign_board_name=:NEW.sign_board_name
+  WHERE sign_board_name=:OLD.sign_board_name;
+END;
+
+
+CREATE OR REPLACE TRIGGER sign_board_have_trigger
+AFTER UPDATE ON sign_board FOR EACH ROW
+BEGIN
+  UPDATE HAVE_BOARD
+  SET sign_board_name=:NEW.sign_board_name
+  WHERE sign_board_name=:OLD.sign_board_name;
+END;
+
+
+CREATE OR REPLACE TRIGGER sb_bossnickname_org_trigger
+AFTER UPDATE ON sign_board FOR EACH ROW
+BEGIN
+  UPDATE organization
+  SET boss_nickname=:NEW.boss_nickname
+  WHERE boss_nickname=:OLD.boss_nickname;
+END;
+
+
+CREATE OR REPLACE TRIGGER sb_bossnickname_invite_trigger
+AFTER UPDATE ON sign_board FOR EACH ROW
+BEGIN
+  UPDATE invitation_management
+  SET boss_nickname=:NEW.boss_nickname
+  WHERE boss_nickname=:OLD.boss_nickname;
+END;
+
+
+CREATE OR REPLACE TRIGGER sb_bossnickname_have_trigger
+AFTER UPDATE ON sign_board FOR EACH ROW
+BEGIN
+  UPDATE HAVE_BOARD
+  SET boss_nickname=:NEW.boss_nickname
+  WHERE boss_nickname=:OLD.boss_nickname;
+END;
