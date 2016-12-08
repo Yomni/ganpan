992140fa02185ee2b06b79c81f3f485fb966c0c8
Kang <Kang@192.168.0.132>
2016-12-08 13:40:31 +0900

    트리거 수정 login 처리

--- ganpan/src/test/resources/trigger.sql
+++ ganpan/src/test/resources/trigger.sql
@@ -8,74 +8,73 @@ DROP TRIGGER sb_bossnickname_have_trigger;
 DROP TRIGGER hb_bossnickname_work_trigger;
 DROP TRIGGER org_bossnickname_work_trigger;
 
-
-CREATE TRIGGER sb_sbname_org_trigger
+CREATE OR REPLACE TRIGGER sb_sbname_org_trigger
 AFTER UPDATE ON sign_board FOR EACH ROW
 BEGIN
   UPDATE organization
   SET sign_board_name=:NEW.sign_board_name
-  WHERE sign_board_name=:OLD.sign_board_name
+  WHERE sign_board_name=:OLD.sign_board_name;
 END;
 
-CREATE TRIGGER sb_sbname_invite_trigger
+CREATE OR REPLACE TRIGGER sb_sbname_invite_trigger
 AFTER UPDATE ON sign_board FOR EACH ROW
 BEGIN
   UPDATE invitation_management
   SET sign_board_name=:NEW.sign_board_name
-  WHERE sign_board_name=:OLD.sign_board_name
+  WHERE sign_board_name=:OLD.sign_board_name;
 END;
 
 
-CREATE TRIGGER sb_have_trigger
+CREATE OR REPLACE TRIGGER sb_have_trigger
 AFTER UPDATE ON sign_board FOR EACH ROW
 BEGIN
   UPDATE HAVE_BOARD
   SET sign_board_name=:NEW.sign_board_name
-  WHERE sign_board_name=:OLD.sign_board_name
+  WHERE sign_board_name=:OLD.sign_board_name;
 END;
 
 
-CREATE TRIGGER sb_bossnickname_invite_trigger
+CREATE OR REPLACE TRIGGER sb_bossnickname_invite_trigger
 AFTER UPDATE ON sign_board FOR EACH ROW
 BEGIN
   UPDATE invitation_management
   SET boss_nickname=:NEW.boss_nickname
-  WHERE boss_nickname=:OLD.boss_nickname
+  WHERE boss_nickname=:OLD.boss_nickname;
 END;
 
 
-CREATE TRIGGER sb_bossnickname_org_trigger
+CREATE OR REPLACE TRIGGER sb_bossnickname_org_trigger
 AFTER UPDATE ON sign_board FOR EACH ROW
 BEGIN
   UPDATE organization
   SET boss_nickname=:NEW.boss_nickname
-  WHERE boss_nickname=:OLD.boss_nickname
+  WHERE boss_nickname=:OLD.boss_nickname;
 END;
 
 
-CREATE TRIGGER sb_bossnickname_have_trigger
+CREATE OR REPLACE TRIGGER sb_bossnickname_have_trigger
 AFTER UPDATE ON sign_board FOR EACH ROW
 BEGIN
   UPDATE have_board
   SET boss_nickname=:NEW.boss_nickname
-  WHERE boss_nickname=:OLD.boss_nickname
+  WHERE boss_nickname=:OLD.boss_nickname;
 END;
 
 
-CREATE TRIGGER hb_bossnickname_work_trigger
+CREATE OR REPLACE TRIGGER hb_bossnickname_work_trigger
 AFTER UPDATE ON have_board FOR EACH ROW
 BEGIN
   UPDATE work
   SET boss_nickname=:NEW.boss_nickname
-  WHERE boss_nickname=:OLD.boss_nickname
+  WHERE boss_nickname=:OLD.boss_nickname;
 END;
 
 
-CREATE TRIGGER org_bossnickname_work_trigger
+CREATE OR REPLACE TRIGGER org_bossnickname_work_trigger
 AFTER UPDATE ON organization FOR EACH ROW
 BEGIN
   UPDATE work
   SET boss_nickname=:NEW.boss_nickname
-  WHERE boss_nickname=:OLD.boss_nickname
+  WHERE boss_nickname=:OLD.boss_nickname;
 END;
 
