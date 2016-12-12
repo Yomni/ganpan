3dc9c1a35e34091c2f723df9d29226dfbdd83754
Kang <Kang@192.168.0.132>
2016-12-12 15:38:59 +0900

    회원 탈퇴 기능 구현 진행중

---
+++ ganpan/src/main/webapp/WEB-INF/views/member/left_template/leave.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<div class="col-md-5 col-md-offset-4">
+<br><br><br><br><br><br>
+	<div class="panel panel-danger">
+	  <div class="panel-heading">
+	    <h2 class="panel-title">회원 탈퇴</h2>
+	  </div>
+	  <div class="panel-body" style="text-align: center;">
+	  	<form action="leave.do" class="form-inline">
+	  		<label for="exampleInputName2">비밀번호</label>
+	    	<input type="password" class="form-control" id="password" name="password"/>
+ 			<button type="submit" class="btn btn-danger">탈퇴하기</button><br>
+	  		<label for="exampleInputName2">※주의 탈퇴 시 보유한 칸반이 모두 삭제됩니다!</label>
+	  	</form>
+	  </div>
+	</div>
+</div>
--- ganpan/src/test/resources/trigger.sql
+++ ganpan/src/test/resources/trigger.sql
@@ -1,3 +1,88 @@
+DROP TRIGGER sb_signboardname_trigger;
+DROP TRIGGER sb_bossnickname_trigger;
+DROP TRIGGER org_bossnickname_trigger;
+DROP TRIGGER org_signboardname_trigger;
+DROP TRIGGER hb_bossnickname_trigger;
+DROP TRIGGER hb_bossnickname_trigger;
+
+
+
+CREATE OR REPLACE TRIGGER sb_signboardname_trigger
+AFTER UPDATE OF sign_board_name ON sign_board FOR EACH ROW
+BEGIN
+  UPDATE organization
+  SET sign_board_name=:NEW.sign_board_name
+  WHERE sign_board_name=:OLD.sign_board_name;
+  UPDATE invitation_management
+  SET sign_board_name=:NEW.sign_board_name
+  WHERE sign_board_name=:OLD.sign_board_name;
+  UPDATE HAVE_BOARD
+  SET sign_board_name=:NEW.sign_board_name
+  WHERE sign_board_name=:OLD.sign_board_name;
+END;
+
+CREATE OR REPLACE TRIGGER sb_bossnickname_trigger
+AFTER UPDATE OF boss_nickname ON sign_board FOR EACH ROW
+BEGIN
+  UPDATE invitation_management
+  SET boss_nickname=:NEW.boss_nickname
+  WHERE boss_nickname=:OLD.boss_nickname;
+  UPDATE organization
+  SET boss_nickname=:NEW.boss_nickname
+  WHERE boss_nickname=:OLD.boss_nickname;
+  UPDATE have_board
+  SET boss_nickname=:NEW.boss_nickname
+  WHERE boss_nickname=:OLD.boss_nickname;
+END;
+
+CREATE OR REPLACE TRIGGER org_bossnickname_trigger
+AFTER UPDATE OF boss_nickname ON organization FOR EACH ROW
+BEGIN
+  UPDATE work
+  SET boss_nickname=:NEW.boss_nickname
+  WHERE boss_nickname=:OLD.boss_nickname;
+END;
+
+CREATE OR REPLACE TRIGGER org_signboardname_trigger
+AFTER UPDATE OF sign_board_name ON organization FOR EACH ROW
+BEGIN
+  UPDATE work
+  SET sign_board_name=:NEW.sign_board_name
+  WHERE sign_board_name=:OLD.sign_board_name;
+END;
+
+CREATE OR REPLACE TRIGGER hb_bossnickname_trigger
+AFTER UPDATE OF boss_nickname ON have_board FOR EACH ROW
+BEGIN
+  UPDATE work
+  SET boss_nickname=:NEW.boss_nickname
+  WHERE boss_nickname=:OLD.boss_nickname;
+END;
+
+CREATE OR REPLACE TRIGGER hb_signboardname_trigger
+AFTER UPDATE OF sign_board_name ON have_board FOR EACH ROW
+BEGIN
+  UPDATE work
+  SET sign_board_name=:NEW.sign_board_name
+  WHERE sign_board_name=:OLD.sign_board_name;
+END;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 DROP TRIGGER sb_sbname_org_trigger;
 DROP TRIGGER sb_sbname_invite_trigger;
